156cfe496f4aeba1dbfe6be276ae7710061026bb
antons2006 <49318768+antons2006@users.noreply.github.com>
2021-07-16 14:46:22 +0300

    Add files via upload

---
+++ CallbackControllercs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CallBackVkBot
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        public CallbackController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Проверяем, что находится в поле "type" 
+            switch (updates.Type)
+            {
+                // Если это уведомление для подтверждения адреса
+                case "confirmation":
+                    // Отправляем строку для подтверждения 
+                    return Ok(_configuration["Config:Confirmation"]);
+            }
+            // Возвращаем "ok" серверу Callback API
+            return Ok("ok");
+        }
+    }
+}

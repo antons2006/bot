60705991f71e839fdb9d91b702f15dc48fb734ce
antons2006 <49318768+antons2006@users.noreply.github.com>
2021-07-16 15:05:17 +0300

    Add files via upload

---
+++ testCallBackApiBot/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testCallBackApiBot.Controllers
+{
+    [Serializable]
+    public class Updates
+    {
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+        [JsonProperty("group_id")]
+        public long GroupId
+        {
+            get; set;
+        }
+    }
+
+   
+}
